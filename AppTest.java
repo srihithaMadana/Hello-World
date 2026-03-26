cd00b2653a0dc249ae4078186bb856c009c8b9af
SrijaGopu05 <24B81A6754@cvr.ac.in>
2026-03-26 13:33:46 +0530

    Initial commit of Java project

---
+++ AppTest.java
@@ -0,0 +1,21 @@
+public class AppTest {
+
+public static void main(String[] args) {
+
+String expected = "Hello";
+
+String actual = "Hello";
+
+if (expected.equals(actual)) {
+
+System.out.println("Test Passed!");
+
+} else {
+
+System.out.println("Test Failed!");
+
+}
+
+}
+
+} 
